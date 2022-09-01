6b707f995e31b39c0eb818968da6c5e03573f16d
oybekdev1998 <oybekjon.shamuratov@mail.ru>
2022-09-01 20:59:22 +0500

    Styled Sidebar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Topbar from "./components/Topbar/Topbar";
+import Sidebar from "./components/Sidebar/Sidebar";
+import styles from './App.module.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Topbar />
+        <div className={styles.container}>
+          <Sidebar />
+          <div className={styles.others}></div>
+        </div>
     </div>
   );
 }
