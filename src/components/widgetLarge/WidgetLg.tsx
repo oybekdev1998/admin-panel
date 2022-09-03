5a2bc1ae455d27c9bec38a562466935166b992f9
oybekdev1998 <oybekjon.shamuratov@mail.ru>
2022-09-03 19:38:09 +0500

    Created WidgetLg component

---
+++ src/components/widgetLarge/WidgetLg.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './WidgetLg.module.css'
+
+const WidgetLg = () => {
+  return (
+    <div className={styles.widgetLg}>
+      widget
+    </div>
+  )
+}
+
+export default WidgetLg
