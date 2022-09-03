651d95c104117caa3223a48b0608adfcb42658b3
oybekdev1998 <oybekjon.shamuratov@mail.ru>
2022-09-03 21:17:46 +0500

    Styled WidgetSm component

--- src/components/widgetSmall/WidgetSm.tsx
+++ src/components/widgetSmall/WidgetSm.tsx
@@ -17,7 +17,52 @@ const WidgetSm = () => {
             <span className={styles.widgetSmUserTitle}>Software Engineer</span>
           </div>
           <button className={styles.widgetSmBtn}>
-            <Visibility />
+            <Visibility className={styles.widgetSmIcon} />
+            Display
+          </button>
+        </li>
+        <li className={styles.widgetSmItem}>
+          <img
+            src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0bprqdveT23zTtuyFKHv8W-CGtaRMn86R4w&usqp=CAU"}
+            alt={'user'}
+            className={styles.widgetSmUserImg}
+          />
+          <div className={styles.widgetSmUser}>
+            <span className={styles.widgetSmUsername}>Anna Keller</span>
+            <span className={styles.widgetSmUserTitle}>Software Engineer</span>
+          </div>
+          <button className={styles.widgetSmBtn}>
+            <Visibility className={styles.widgetSmIcon} />
+            Display
+          </button>
+        </li>
+        <li className={styles.widgetSmItem}>
+          <img
+            src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0bprqdveT23zTtuyFKHv8W-CGtaRMn86R4w&usqp=CAU"}
+            alt={'user'}
+            className={styles.widgetSmUserImg}
+          />
+          <div className={styles.widgetSmUser}>
+            <span className={styles.widgetSmUsername}>Anna Keller</span>
+            <span className={styles.widgetSmUserTitle}>Software Engineer</span>
+          </div>
+          <button className={styles.widgetSmBtn}>
+            <Visibility className={styles.widgetSmIcon} />
+            Display
+          </button>
+        </li>
+        <li className={styles.widgetSmItem}>
+          <img
+            src={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0bprqdveT23zTtuyFKHv8W-CGtaRMn86R4w&usqp=CAU"}
+            alt={'user'}
+            className={styles.widgetSmUserImg}
+          />
+          <div className={styles.widgetSmUser}>
+            <span className={styles.widgetSmUsername}>Anna Keller</span>
+            <span className={styles.widgetSmUserTitle}>Software Engineer</span>
+          </div>
+          <button className={styles.widgetSmBtn}>
+            <Visibility className={styles.widgetSmIcon} />
             Display
           </button>
         </li>
