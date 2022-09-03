7274dacc6e36bc31ff6a94646674ae4962036748
oybekdev1998 <oybekjon.shamuratov@mail.ru>
2022-09-03 18:46:46 +0500

    Created helpers and added data.ts

---
+++ src/helpers/data.ts
@@ -0,0 +1,54 @@
+
+export const data = [
+  {
+    name: 'Jan',
+    "Active User": 2500
+
+  },
+  {
+    name: 'Feb',
+    "Active User": 3000
+
+  },
+  {
+    name: 'March',
+    "Active User": 2000
+  },
+  {
+    name: 'Apr',
+    "Active User": 2780
+  },
+  {
+    name: 'May',
+    "Active User": 1890
+  },
+  {
+    name: 'Jun',
+    "Active User": 2390
+  },
+  {
+    name: 'Jul',
+    "Active User": 3490
+  },
+  {
+    name: 'Avg',
+    "Active User": 1800
+  },
+  {
+    name: 'Sep',
+    "Active User": 2100
+  },
+  {
+    name: 'Oct',
+    "Active User": 2780
+  },
+  {
+    name: 'Nov',
+    "Active User": 2900
+  },
+  {
+    name: 'Dec',
+    "Active User": 3600
+  },
+
+];
