ace25b718ae24d6caa8cd6600b3a840dde133d6a
oybekdev1998 <oybekjon.shamuratov@mail.ru>
2022-09-03 14:55:55 +0500

    Added Home page

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React, {FC} from 'react';
+import styles from './Home.module.css';
+
+const Home: FC = () => {
+  return (
+    <div className={styles.home}>
+      home
+    </div>
+  )
+}
+
+export default Home;
