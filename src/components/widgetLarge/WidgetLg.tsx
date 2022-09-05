56cf32a92f8fe9f2acb9e3bae79b6123130905e1
oybekdev1998 <oybekjon.shamuratov@mail.ru>
2022-09-05 15:30:37 +0500

    Add widgetLg

--- src/components/widgetLarge/WidgetLg.tsx
+++ src/components/widgetLarge/WidgetLg.tsx
@@ -4,7 +4,15 @@ import styles from './WidgetLg.module.css'
 const WidgetLg = () => {
   return (
     <div className={styles.widgetLg}>
-      widget
+      <h3 className={styles.widgetLgTitle}>Latest transaction</h3>
+      <table className={styles.widgetLgTable}>
+        <tr className={styles.widgetLgRow}>
+          <th className={styles.widgetLgTh}>Customer</th>
+          <th className={styles.widgetLgTh}>Date</th>
+          <th className={styles.widgetLgTh}>Amount</th>
+          <th className={styles.widgetLgTh}>Status</th>
+        </tr>
+      </table>
     </div>
   )
 }
