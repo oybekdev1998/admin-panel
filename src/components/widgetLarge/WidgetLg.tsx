5d44b6dd3d4f516b721bd891e173c9d5806db430
oybekdev1998 <oybekjon.shamuratov@mail.ru>
2022-09-06 16:01:48 +0500

    Finished style and functional widgetLg

--- src/components/widgetLarge/WidgetLg.tsx
+++ src/components/widgetLarge/WidgetLg.tsx
@@ -12,6 +12,52 @@ const WidgetLg = () => {
           <th className={styles.widgetLgTh}>Amount</th>
           <th className={styles.widgetLgTh}>Status</th>
         </tr>
+
+        <tr className={styles.widgetLgRow}>
+          <td className={styles.widgetLgUser}>
+            <td className={styles.widgetLgUserInfo}>
+              <img src={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQauTp_GTvoNQVQV19rVJJwdAfES8AnV1qKRw&usqp=CAU'} className={styles.widgetLgUserImg} alt={'user'}/>
+              <span className={styles.widgetLgUsername}>Susan Carol</span>
+            </td>
+            <td className={styles.widgetLgDate}>2 Jun 2021</td>
+            <td className={styles.widgetLgMoney}>$122.00</td>
+            <td className={styles.widgetLgStatus}> <button className={styles.widgetLgBtn}>Approved</button> </td>
+          </td>
+        </tr>
+        <tr className={styles.widgetLgRow}>
+          <td className={styles.widgetLgUser}>
+            <td className={styles.widgetLgUserInfo}>
+              <img src={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQauTp_GTvoNQVQV19rVJJwdAfES8AnV1qKRw&usqp=CAU'} className={styles.widgetLgUserImg} alt={'user'}/>
+              <span className={styles.widgetLgUsername}>Jessica Carol</span>
+            </td>
+            <td className={styles.widgetLgDate}>2 Jun 2021</td>
+            <td className={styles.widgetLgMoney}>$122.00</td>
+            <td className={styles.widgetLgStatus}> <button className={styles.widgetLgBtn}>Pending</button> </td>
+          </td>
+        </tr>
+        <tr className={styles.widgetLgRow}>
+          <td className={styles.widgetLgUser}>
+            <td className={styles.widgetLgUserInfo}>
+              <img src={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQauTp_GTvoNQVQV19rVJJwdAfES8AnV1qKRw&usqp=CAU'} className={styles.widgetLgUserImg} alt={'user'}/>
+              <span className={styles.widgetLgUsername}>Matilda Carol</span>
+            </td>
+            <td className={styles.widgetLgDate}>2 Jun 2021</td>
+            <td className={styles.widgetLgMoney}>$122.00</td>
+            <td className={styles.widgetLgStatus}> <button className={styles.widgetLgBtn}>Rejected</button> </td>
+          </td>
+        </tr>
+        <tr className={styles.widgetLgRow}>
+          <td className={styles.widgetLgUser}>
+            <td className={styles.widgetLgUserInfo}>
+              <img src={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQauTp_GTvoNQVQV19rVJJwdAfES8AnV1qKRw&usqp=CAU'} className={styles.widgetLgUserImg} alt={'user'}/>
+              <span className={styles.widgetLgUsername}>Monica Carol</span>
+            </td>
+            <td className={styles.widgetLgDate}>2 Jun 2021</td>
+            <td className={styles.widgetLgMoney}>$122.00</td>
+            <td className={styles.widgetLgStatus}> <button className={styles.widgetLgBtn}>Approved</button> </td>
+          </td>
+        </tr>
+
       </table>
     </div>
   )
