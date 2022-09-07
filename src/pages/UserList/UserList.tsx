2af91b13ad9fcde45c94efb1b93d4fb492178b5c
oybekdev1998 <oybekjon.shamuratov@mail.ru>
2022-09-07 22:21:50 +0500

    Finished style in userList component

--- src/pages/UserList/UserList.tsx
+++ src/pages/UserList/UserList.tsx
@@ -1,55 +1,63 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
 import { rows } from "../../helpers/tableData";
 import styles from './UserList.module.css'
 import {DeleteOutline} from "@mui/icons-material";
-
-const columns: GridColDef[] = [
-  { field: 'id', headerName: 'ID', width: 90 },
-  { field: 'user', headerName: 'User', width: 200, renderCell: (params)=> {
-    return (
-      <div className={styles.userListUser}>
-        <img className={styles.userListImg} src={params.row.avatar} alt='user' />
-        {params.row.user}
-      </div>
-    )
-    } },
-  { field: 'email', headerName: 'Email', width: 200 },
-  {
-    field: 'status',
-    headerName: 'Status',
-    type: 'string',
-    width: 120,
-  },
-  {
-    field: 'transaction',
-    headerName: 'Transaction',
-    type: 'string',
-    width: 120,
-  },
-  {
-    field: 'action',
-    headerName: 'Action',
-    width: 200,
-    renderCell: (params) => {
-      return (
-        <>
-        <button className={styles.userListBtn}>Edit</button>
-        <DeleteOutline className={styles.userListDelete} />
-        </>
-      )
-    }
-  }
-
-];
-
+import {Link} from "react-router-dom";
 
 
 function UserList() {
+  const [data, setData] = useState<Array<any>>(rows)
+
+  const handleClick = (id: number) => {
+    setData(rows.filter(item => item.id !== id))
+  }
+
+  const columns: GridColDef[] = [
+    { field: 'id', headerName: 'ID', width: 90 },
+    { field: 'user', headerName: 'User', width: 200, renderCell: (params)=> {
+        return (
+          <div className={styles.userListUser}>
+            <img className={styles.userListImg} src={params.row.avatar} alt='user' />
+            {params.row.user}
+          </div>
+        )
+      } },
+    { field: 'email', headerName: 'Email', width: 200 },
+    {
+      field: 'status',
+      headerName: 'Status',
+      type: 'string',
+      width: 120,
+    },
+    {
+      field: 'transaction',
+      headerName: 'Transaction',
+      type: 'string',
+      width: 120,
+    },
+    {
+      field: 'action',
+      headerName: 'Action',
+      width: 200,
+      renderCell: (params) => {
+        return (
+          <>
+            <Link to={'/user/' + params.row.id}>
+              <button className={styles.userListBtn}>Edit</button>
+            </Link>
+            <DeleteOutline className={styles.userListDelete} onClick={() => handleClick(params.row.id)} />
+          </>
+        )
+      }
+    }
+  ];
+
+
   return (
     <div className={styles.userList} style={{ height: '100vh', width: '100%' }}>
       <DataGrid
-        rows={rows}
+        rows={data}
         columns={columns}
         pageSize={10}
         checkboxSelection
@@ -58,5 +66,10 @@ function UserList() {
     </div>
   );
 }
+
+
+
+
+
 
 export default UserList;
