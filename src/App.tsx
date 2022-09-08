0f26a4365235fb743cb294ac8907fbc4fef414c8
oybekdev1998 <oybekjon.shamuratov@mail.ru>
2022-09-08 22:35:58 +0500

    Added router to product

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import {Route, Routes,} from "react-router-dom";
 import Topbar from "./components/Topbar/Topbar";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Home from "./pages/Home/Home";
-import {Route, Routes,} from "react-router-dom";
 import UserList from "./pages/UserList/UserList";
 import User from "./pages/User/User";
 import NewUser from "./pages/NewUser/NewUser";
+import ProductList from "./pages/ProductList/ProductList";
+import Product from "./pages/Product/Product";
 
 import styles from './App.module.css'
 
@@ -20,6 +22,8 @@ function App() {
             <Route path={'/userList'} element={<UserList />} />
             <Route path={'/User/:userId'} element={<User /> } />
             <Route path={'/newUser'} element={<NewUser />}/>
+            <Route path={'/products'} element={<ProductList />} />
+            <Route path={'/products/:productId'} element={<Product />}/>
           </Routes>
 
         </div>
