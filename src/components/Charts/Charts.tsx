c4997807f526256e175996a1698708432cc97060
oybekdev1998 <oybekjon.shamuratov@mail.ru>
2022-09-09 14:33:09 +0500

    Fixed bag in charts

--- src/components/Charts/Charts.tsx
+++ src/components/Charts/Charts.tsx
@@ -4,11 +4,10 @@ import {data} from "../../helpers/data";
 import styles from './Charts.module.css'
 
 const Charts = () => {
-
   return (
     <div className={styles.charts} >
       <h3 className={styles.chartTitle}>Sales Analytics</h3>
-      <ResponsiveContainer width="100%" aspect={4 / 1} >
+      <ResponsiveContainer width="100%" aspect={4} >
                 <LineChart data={data}>
                   <XAxis dataKey="name" stroke="#5550bd" />
                   <Line type="monotone" dataKey="Active User" stroke="#5550bd"  />
